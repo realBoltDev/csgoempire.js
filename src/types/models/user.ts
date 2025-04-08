66f66306333742e0522d0cc26d4a5e66ba29ee0f
beastxbolt <harsit2405@gmail.com>
2025-04-08 15:57:07 +0530

    feat: Add interface

---
+++ src/types/models/user.ts
@@ -0,0 +1,9 @@
+export interface User {
+  steam_id: string;
+  steam_name: string;
+  avatar: string;
+  avatar_full: string;
+  profile_url: string;
+  timecreated: number;
+  steam_level: number;
+}
